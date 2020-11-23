70a0abd20ea56455b747758633bac8494d700c5e
Tomas Patro <tomas.patro@gmail.com>
2020-11-23 23:35:12 +0100

    Add basic queue methods

--- syn_queue/syn_queue.py
+++ syn_queue/syn_queue.py
@@ -1,16 +1,39 @@
-import threading as thr
+import threading
+from collections import deque
 
 from syn_queue.errors.queue_error import QueueError
 from syn_queue.errors.argument_error import ArgumentError
 
 
-class Queue():
+class SynQueue():
     def __init__(self, max_size=0):
+        self.__init_attrs(max_size)
+        self.__init_threading()
+
+    def size(self):
+        with self.__lock:
+            return self.__size()
+
+    def empty(self):
+        with self.__lock:
+            return self.__size() == 0
+
+    def full(self):
+        with self.__lock:
+            return self.__size() == self.__max_size()
+
+    def __init_attrs(self, max_size):
         self.__max_size = max_size
         self.__validate_max_size()
         self.__infinite = max_size <= 0
-        self.__queue = []
+        self.__queue = deque(max_size)
+
+    def __init_threading(self):
+        self.__lock = threading.Lock()
 
     def __validate_max_size(self):
         if not isinstance(self.__max_size, int):
             raise ArgumentError
+
+    def __size(self):
+        return len(self.__queue)
--- tests/test_syn_queue.py
+++ tests/test_syn_queue.py
@@ -1,18 +1,18 @@
 import pytest
 
-from syn_queue.syn_queue import Queue
+from syn_queue.syn_queue import SynQueue
 from syn_queue.errors.argument_error import ArgumentError
 
 
 class TestSynQueue():
     def test_init(self):
         with pytest.raises(ArgumentError):
-            queue = Queue(max_size="err")
+            queue = SynQueue(max_size="err")
         
-        queue = Queue()
-        assert queue._Queue__max_size == 0
-        assert queue._Queue__infinite == True
+        queue = SynQueue()
+        assert queue._SynQueue__max_size == 0
+        assert queue._SynQueue__infinite == True
 
-        queue = Queue(max_size=100)
-        assert queue._Queue__max_size == 100
-        assert queue._Queue__infinite == False
+        queue = SynQueue(max_size=100)
+        assert queue._SynQueue__max_size == 100
+        assert queue._SynQueue__infinite == False
